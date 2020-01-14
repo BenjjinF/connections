757293dd69b0f0ae98afc3aca080b3ce2482d240
Benjnamin Frost <info@btfrost.com>
2020-01-13 21:39:38 -0400

    Implemented functional test to validate connection creation

--- tests/functional/connections/test_create_connection.py
+++ tests/functional/connections/test_create_connection.py
@@ -1,20 +1,26 @@
 from http import HTTPStatus
 
+import pytest
 from tests.factories import PersonFactory
 
 from connections.models.connection import Connection
 
 
-def test_can_create_connection(db, testapp):
+@pytest.fixture
+def connection_payload(db):
     person_from = PersonFactory(first_name='Diana')
     person_to = PersonFactory(first_name='Harry')
     db.session.commit()
-    payload = {
+    return {
         'from_person_id': person_from.id,
         'to_person_id': person_to.id,
         'connection_type': 'mother',
     }
-    res = testapp.post('/connections', json=payload)
+
+
+def test_can_create_connection(db, testapp, connection_payload):
+
+    res = testapp.post('/connections', json=connection_payload)
 
     assert res.status_code == HTTPStatus.CREATED
 
@@ -23,6 +29,26 @@ def test_can_create_connection(db, testapp):
     connection = Connection.query.get(res.json['id'])
 
     assert connection is not None
-    assert connection.from_person_id == person_from.id
-    assert connection.to_person_id == person_to.id
+    assert connection.from_person_id == connection_payload['from_person_id']
+    assert connection.to_person_id == connection_payload['to_person_id']
     assert connection.connection_type.value == 'mother'
+
+
+@pytest.mark.parametrize('field, value, error_message', [
+    pytest.param('from_person_id', None, 'Field may not be null.', id='missing from person'),
+    pytest.param('to_person_id', 'Diane', 'Not a valid integer.', id='incorrect from person'),
+    pytest.param('to_person_id', None, 'Field may not be null.', id='missing to person'),
+    pytest.param('to_person_id', 'Harry', 'Not a valid integer.', id='incorrect to person'),
+    pytest.param('connection_type', None, 'Field may not be null.', id='missing type'),
+    pytest.param('connection_type', 'friendz', 'Invalid enum member friendz', id='incorrect type'),
+])
+def test_create_connection_validations(
+        db, testapp, connection_payload, field, value, error_message):
+    connection_payload[field] = value
+
+    res = testapp.post('/connections', json=connection_payload)
+
+    assert res.status_code == HTTPStatus.BAD_REQUEST
+    assert res.json['description'] == 'Input failed validation.'
+    errors = res.json['errors']
+    assert error_message in errors[field]
