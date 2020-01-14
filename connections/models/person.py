53c9b648c3ee1995414d97a3b77a023f2c830142
Benjnamin Frost <info@btfrost.com>
2020-01-13 21:03:53 -0400

    Implemented test_mutual_friends & Person.mutual_friends

--- connections/models/person.py
+++ connections/models/person.py
@@ -8,3 +8,15 @@ class Person(Model, CRUDMixin, CreatedUpdatedMixin):
     email = db.Column(db.String(145), unique=True, nullable=False)
 
     connections = db.relationship('Connection', foreign_keys='Connection.from_person_id')
+
+    def mutual_friends(self, target):
+        person_friends = [
+            friend.to_person_id for friend in self.connections
+            if friend.connection_type.name == 'friend'
+        ]
+        target_friends = [
+            friend.to_person_id for friend in target.connections
+            if friend.connection_type.name == 'friend'
+        ]
+        mutual_friends = Person.query.filter(Person.id.in_([*person_friends, *target_friends]))
+        return [friend for friend in mutual_friends]
--- tests/units/test_person.py
+++ tests/units/test_person.py
@@ -2,24 +2,26 @@ import pytest
 from tests.factories import ConnectionFactory, PersonFactory
 
 
-@pytest.mark.xfail
 def test_mutual_friends(db):
     instance = PersonFactory()
     target = PersonFactory()
 
     # some decoy connections (not mutual)
-    ConnectionFactory.create_batch(5, to_person=instance)
-    ConnectionFactory.create_batch(5, to_person=target)
+    ConnectionFactory.create_batch(5, to_person_id=instance.id, from_person_id=None)
+    ConnectionFactory.create_batch(5, to_person_id=target.id, from_person_id=None)
 
     mutual_friends = PersonFactory.create_batch(3)
+
+    db.session.commit()
+
     for f in mutual_friends:
-        ConnectionFactory(from_person=instance, to_person=f, connection_type='friend')
-        ConnectionFactory(from_person=target, to_person=f, connection_type='friend')
+        ConnectionFactory(from_person_id=instance.id, to_person_id=f.id, connection_type='friend')
+        ConnectionFactory(from_person_id=target.id, to_person_id=f.id, connection_type='friend')
 
     # mutual connections, but not friends
     decoy = PersonFactory()
-    ConnectionFactory(from_person=instance, to_person=decoy, connection_type='coworker')
-    ConnectionFactory(from_person=target, to_person=decoy, connection_type='coworker')
+    ConnectionFactory(from_person_id=instance.id, to_person_id=decoy.id, connection_type='coworker')
+    ConnectionFactory(from_person_id=target.id, to_person_id=decoy.id, connection_type='coworker')
 
     db.session.commit()
 
